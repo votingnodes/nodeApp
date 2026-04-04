c0ae7598ca90e46d06e39626b86ca7fd71ef5155
Security Brahh <securitybrahh@empiresec.co>
2026-04-04 10:36:50 +0530

    removed jsmain, jvmmain, wasmjsmain

--- shared/src/jvmMain/kotlin/co/empiresec/nodeapp/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package co.empiresec.nodeapp
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
